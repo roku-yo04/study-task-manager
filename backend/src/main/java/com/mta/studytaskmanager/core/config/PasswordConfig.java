833aae176dfd2650b89cdaede9c91053916783c3
Loc Tran <tanloc0124.february@gmai.com>
2026-04-20 23:35:30 +0700

    set up Bcrypt password encoder bean

--- backend/src/main/java/com/mta/studytaskmanager/core/config/PasswordConfig.java
+++ backend/src/main/java/com/mta/studytaskmanager/core/config/PasswordConfig.java
@@ -1,5 +1,14 @@
 package com.mta.studytaskmanager.core.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+@Configuration
 public class PasswordConfig {
     // Để có PasswordEncoder dùng trong Auth sau này.
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
